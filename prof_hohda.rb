b5d1e87fbb33b559dd5418bf678e67ab2dbfd73f
KenichiroHohda <snggtd@gmail.com>
2016-09-19 07:36:09 +0000

    make prof

---
+++ prof_hohda.rb
@@ -0,0 +1,22 @@
+require 'sinatra'
+
+get '/' do
+<<EOS
+<html>
+<head>
+  <title>自己紹介</html>
+  
+</head>
+<body>
+  <h1>自己紹介</h1>
+  <hr>
+  <ul>
+    <li>名前　:　宝田憲一郎  </li>
+    <li>住所　：　千葉県 </li>
+    <li>星座　：　しし座 </li>
+  </ul>
+
+</body>
+</html>
+EOS
+end
