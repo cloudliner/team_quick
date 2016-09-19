0f1922cf572795c20b3d203de6515f68cdb358fc
KenichiroHohda <snggtd@gmail.com>
2016-09-19 07:47:10 +0000

    make prof

--- views/prof_hohda.rb
+++ views/prof_hohda.rb
@@ -4,7 +4,7 @@ get '/' do
 <<EOS
 <html>
 <head>
-  <title>自己紹介</html>
+  <title>自己紹介</title>
   
 </head>
 <body>
@@ -15,7 +15,7 @@ get '/' do
     <li>住所　：　千葉県 </li>
     <li>星座　：　しし座 </li>
   </ul>
-
+  <h2><a href="./index.erb">戻る</a></h2>
 </body>
 </html>
 EOS
